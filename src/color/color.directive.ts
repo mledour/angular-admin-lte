2bc0a6bdac59bcd2c4d87d40e992cd7fcb61297b
Mik@ <michael.ledour@gmail.com>
2018-03-06 09:13:27 +0100

    color service

--- src/color/color.directive.ts
+++ src/color/color.directive.ts
@@ -1,81 +1,51 @@
 import { Directive, Input, Renderer2, ElementRef } from '@angular/core';
 
 import { colors, colorsAliases } from './color.definition';
+import { ColorService } from './color.service';
 
 @Directive({
-  selector: '[mkColor]'
+  selector: '[mkColor]',
+  providers: [ColorService]
 })
 export class BackgroundColorDirective {
-  private currentStyle: any;
-  private currentClass: any;
-
+  /**
+   * @method constructor
+   * @param  elementRef   [description]
+   * @param  renderer2    [description]
+   * @param  colorService [description]
+   */
   constructor(
     private elementRef: ElementRef,
-    private renderer2: Renderer2
+    private renderer2: Renderer2,
+    private colorService: ColorService
   ) {}
 
   @Input('mkColorCondition') condition = true;
   @Input('mkColorPrefix') prefix: string;
   @Input('mkColorProperty') property: string;
   @Input('mkColor') set color(color: string) {
-    if(color && this.condition) {
-      this.reset();
-      if(colors[color]) {
-        this.renderer2.addClass(this.elementRef.nativeElement, 'bg-color');
-        this.currentStyle = {property: this.property, color: colors[color]};
-        this.renderer2.setStyle(this.elementRef.nativeElement, this.property, colors[color]);
-      } else {
-        this.renderer2.removeClass(this.elementRef.nativeElement, 'bg-color');
-        if(color.startsWith('#') || color.startsWith('rgb')) {
-          this.currentStyle = {property: this.property, color: color};
-          this.renderer2.setStyle(this.elementRef.nativeElement, this.property, color);
-        } else if(colorsAliases.indexOf(color) !== -1) {
-          this.currentClass = `${this.prefix}-${color}`;
-          this.renderer2.addClass(this.elementRef.nativeElement, this.currentClass);
-        }
-      }
-    }
-  }
-
-  private reset() {
-    if(this.currentStyle) {
-      this.renderer2.removeStyle(this.elementRef.nativeElement, this.currentStyle.property, this.currentStyle.color);
-    } else if(this.currentClass) {
-      this.renderer2.removeClass(this.elementRef.nativeElement, this.currentClass);
-    }
+    this.colorService.setBackgroundColor(color, this.condition, this.property, this.prefix);
   }
 }
 
 @Directive({
-  selector: '[mkFontColor]'
+  selector: '[mkFontColor]',
+  providers: [ColorService]
 })
 export class ColorDirective {
-  private currentStyle: any;
-  private currentClass: any;
-
+  /**
+   * @method constructor
+   * @param  elementRef   [description]
+   * @param  renderer2    [description]
+   * @param  colorService [description]
+   */
   constructor(
     private elementRef: ElementRef,
-    private renderer2: Renderer2
+    private renderer2: Renderer2,
+    private colorService: ColorService
   ) {}
 
   @Input('mkFontColor') set color(color: string) {
-    if(color) {
-      this.reset();
-      if(color.startsWith('#') || color.startsWith('rgb')) {
-        this.currentStyle = color;
-        this.renderer2.setStyle(this.elementRef.nativeElement, 'color', color);
-      } else {
-        this.currentClass = `text-${color}`;
-        this.renderer2.addClass(this.elementRef.nativeElement, this.currentClass);
-      }
-    }
-  }
-
-  private reset() {
-    if(this.currentStyle) {
-      this.renderer2.removeStyle(this.elementRef.nativeElement, 'color', this.currentStyle);
-    } else if(this.currentClass) {
-      this.renderer2.removeClass(this.elementRef.nativeElement, this.currentClass);
-    }
+    this.colorService.setFontColor(color);
   }
 }
