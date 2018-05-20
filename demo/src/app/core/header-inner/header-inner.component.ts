404822cfc4ab862dbfa5fdd6f81e8d19573c07e6
Mik@ <michael.ledour@gmail.com>
2018-05-20 17:09:32 +0200

    clean

--- demo/src/app/core/header-inner/header-inner.component.ts
+++
@@ -1,6 +0,0 @@
-import { Component } from '@angular/core';
-@Component({
-  selector: 'app-header-inner',
-  templateUrl: './header-inner.component.html'
-})
-export class HeaderInnerComponent {}
--- demo/src/app/core/sidebar-left-inner/sidebar-left-inner.component.ts
+++
@@ -1,7 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-sidebar-left-inner',
-  templateUrl: './sidebar-left-inner.component.html'
-})
-export class SidebarLeftInnerComponent {}
