456484c84eccf1e2f3a751a0edbf76a3ac8a279a
mika-el <michael.ledour@gmail.com>
2018-05-26 15:33:17 +0200

    clean error static with lambda

--- library/angular-admin-lte/src/lib/services/routing.service.ts
+++ library/angular-admin-lte/src/lib/services/routing.service.ts
@@ -43,7 +43,8 @@ export class RoutingService {
    * @return [description]
    */
   static createUrl(route: ActivatedRouteSnapshot): string {
-    return route.url.map(urlSegment => urlSegment.toString()).join('/');
+    const url = route.url.map(urlSegment => urlSegment.toString()).join('/');
+    return url;
   }
 
   /**
