8536d835a05a2782e654e68c5d584e875bf4bb96
mika <michael.ledour@gmail.com>
2018-05-30 14:59:33 +0200

    input group color

--- library/angular-admin-lte/src/lib/form/input-group/input-group.component.ts
+++ library/angular-admin-lte/src/lib/form/input-group/input-group.component.ts
@@ -1,4 +1,6 @@
-import {AfterContentInit, Component, ContentChild, Input} from '@angular/core';
+import { AfterContentInit, Component, ContentChild, Input, OnDestroy } from '@angular/core';
+
+import { removeSubscriptions } from '../../helpers';
 
 import {
   InputGroupAddonLeftDirective,
@@ -8,6 +10,8 @@ import {
 } from './input-group.directive';
 
 import {InputTextDirective} from '../input-text/input-text.directive';
+import { NgControl } from '@angular/forms';
+import { Subscription } from 'rxjs';
 
 
 /*
@@ -18,23 +22,29 @@ import {InputTextDirective} from '../input-text/input-text.directive';
   templateUrl: './input-group.component.html',
   styleUrls: ['./input-group.component.css']
 })
-export class InputGroupComponent implements AfterContentInit {
+export class InputGroupComponent implements AfterContentInit, OnDestroy {
+  private subscriptions: Array<Subscription> = [];
 
   @Input() addonLeft: string;
   @Input() addonRight: string;
   @Input() inputColor: string;
   @Input() inputFontColor: string;
   @Input() label: string;
-  @Input() wrapperClasses = 'input-group';
+  @Input() wrapperClasses = 'form-group';
 
   @ContentChild(InputGroupLabelDirective) public inputGroupLabelDirective: InputGroupLabelDirective;
   @ContentChild(InputGroupAddonLeftDirective) public inputGroupAddonLeftDirective: InputGroupAddonLeftDirective;
   @ContentChild(InputGroupAddonRightDirective) public inputGroupAddonRightDirective: InputGroupAddonRightDirective;
   @ContentChild(InputGroupContentDirective) public inputGroupContentDirective: InputGroupContentDirective;
-
   @ContentChild(InputTextDirective) public inputTextDirective: InputTextDirective;
 
   ngAfterContentInit() {
-    console.log(this.inputTextDirective);
+    this.subscriptions.push(this.inputTextDirective.onKeyup.subscribe((value: NgControl) => {
+      console.log(value);
+    }));
+  }
+
+  ngOnDestroy() {
+    removeSubscriptions(this.subscriptions);
   }
 }
