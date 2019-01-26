6777b4219a19d07f5e6d6032e5719af29225c8b5
mledour <michael.ledour@atinternet.com>
2019-01-26 23:14:52 +0100

    misc(travis): handle gh-pages sub folder

--- scripts/gh-pages.sh
+++ scripts/gh-pages.sh
@@ -1,17 +1,10 @@
 #!/bin/bash
 
 tmpDir=$1
-src=$2
+srcDir=$2
 
-remote=`git config --get remote.origin.url`
-
-if [ "$TRAVIS_PULL_REQUEST" != false ] ; then
-  baseTarget="pulls"
-  target="pulls/$TRAVIS_PULL_REQUEST"
-else
-  baseTarget="branches"
-  target="branches/$TRAVIS_BRANCH"
-fi
+oldPath="$tmpDir/old"
+newPath="$tmpDir/new"
 
 # Get the opened pull requests
 jsonPulls=$(curl -k -H "Authorization: token $GITHUB_TOKEN" https://api.github.com/repos/${TRAVIS_REPO_SLUG}/pulls?state=open)
@@ -21,53 +14,43 @@ arrPulls=$(jq -n "$jsonPulls" | jq -r '.[].number')
 jsonBranches=$(curl -k -H "Authorization: token $GITHUB_TOKEN" https://api.github.com/repos/${TRAVIS_REPO_SLUG}/branches?state=open)
 arrBranches=$(jq -n "$jsonBranches" | jq -r '.[].name')
 
-# Test inArray function
-inArray() {
-    local haystack=${1}[@]
-    local needle=${2}
-    for i in ${!haystack}; do
-        if [[ ${i} == ${needle} ]]; then
-            return 0
-        fi
-    done
-    return 1
-}
-
 # Clone gh-pages to tmpDir
-git clone --quiet --single-branch -b gh-pages https://${GITHUB_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git ${tmpDir}
-cd $tmpDir
+git clone --quiet --single-branch -b gh-pages https://${GITHUB_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git ${oldPath}
 
-# Clean pull request folders
-for f in pulls/*; do
-    if [ -d ${f} ]; then
-        echo $f
-        if ! inArray arrPulls ${f##*/} ; then
-          echo "Deleting outdated pull request #${f##*/}"
-          rm -R $f
-        fi
-    fi
-done
-
-# Clean branches folders
-echo $arrBranches
-
-for f in branches/*; do
-    if [ -d ${f} ]; then
-        echo $f
-        if ! inArray arrBranches ${f##*/} ; then
-          echo "Deleting outdated branch #${f##*/}"
-          rm -R $f
-        fi
-    fi
-done
-
-echo "tmpDir: $tmpDir"
-echo "target: $target"
-echo "src: $src"
-
+# Clean old git folder
+if [ "$TRAVIS_PULL_REQUEST" != false ] ; then
+  target="pulls/$TRAVIS_PULL_REQUEST"
+else
+  target="branches/$TRAVIS_BRANCH"
+fi
+cd $oldPath
 eval "git filter-branch --tree-filter 'rm -rf $target' --prune-empty HEAD"
-eval "mkdir -p $tmpDir/$target"
-eval "cp -Rp $src/* $tmpDir/$target"
+
+# Create new folder and copy git conf
+mkdir -p ${newPath}
+cp -R ${oldPath}/.git ${newPath}/.git
+
+# Copy branches folders
+for branch in $arrBranches; do
+  [[ -d ${oldPath}/branches/${branch} ]] &&
+  mkdir -p ${newPath}/branches/${branch} &&
+  cp -R ${oldPath}/branches/${branch}/* ${newPath}/branches/${branch}
+done
+
+# Copy pulls folders
+for pull in $arrPulls; do
+  [[ -d ${oldPath}/pulls/${pull} ]] &&
+  mkdir -p ${newPath}/pulls/${pull} &&
+  cp -R ${oldPath}/pulls/${pull}/* ${newPath}/pulls/${pull}
+done
+
+# Add new build to repo
+mkdir -p ${newPath}/${target}
+cp -Rp ${srcDir}/* ${newPath}/${target}
+
+# Push changes
+cd $newPath
 git add -A
 git commit -m "Travis commit"
 git push --force -u origin gh-pages
+
