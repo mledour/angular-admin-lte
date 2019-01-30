fb60c4bd681c8d8c498f7665565122b0bb7cff64
Michael Le Dour <michael.ledour@atinternet.com>
2019-01-30 13:37:10 +0100

    chore(release): 1.3.0

--- library/angular-admin-lte/src/lib/layout/header/header.module.ts
+++ library/angular-admin-lte/src/lib/layout/header/header.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 import { HeaderComponent, HeaderLogoComponent, HeaderLogoMiniComponent } from './header.component';
 
+
 @NgModule({
-    imports: [CommonModule],
+    imports: [CommonModule, RouterModule ],
     exports: [HeaderComponent, HeaderLogoComponent, HeaderLogoMiniComponent],
     declarations: [HeaderComponent, HeaderLogoComponent, HeaderLogoMiniComponent]
 })
