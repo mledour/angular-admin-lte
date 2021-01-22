4f83cab6f78886acbd3b4afd89b2a30b49928e04
mledour <michael.ledour@gmail.com>
2021-01-22 14:54:54 +0100

    style: fix breadcrumbs ts errors

--- library/angular-admin-lte/src/lib/breadcrumbs/breadcrumbs.component.ts
+++ library/angular-admin-lte/src/lib/breadcrumbs/breadcrumbs.component.ts
@@ -1,53 +1,44 @@
 import { Component, OnInit, Input, OnDestroy, ChangeDetectorRef } from '@angular/core';
+
 import { Subscription } from 'rxjs';
 
 import { Path, RoutingService } from '../services/routing.service';
 
-export interface Breadcrumb extends Path {
+
+export interface Breadcrumbs extends Path {
   data: {
     breadcrumbs?: string;
-    title?: string;
+    title?: string
   };
 }
 
-/*
- *
- */
+
 @Component({
   selector: 'mk-breadcrumbs',
   templateUrl: './breadcrumbs.component.html',
   styleUrls: ['./breadcrumbs.component.css']
 })
 export class BreadcrumbsComponent implements OnInit, OnDestroy {
-  public breadcrumbs?: Breadcrumb[];
-
-  private subscription!: Subscription;
-
   @Input() public homeIcon = 'fa fa-home';
 
-  /**
-   * @method constructor
-   * @param routingService [description]
-   * @param changeDetectorRef [description]
-   */
+  public breadcrumbs?: Breadcrumbs [];
+
+  private subscription?: Subscription;
+
   constructor(
     private routingService: RoutingService,
     private changeDetectorRef: ChangeDetectorRef
   ) {}
 
-  /**
-   * @method ngOnInit
-   */
   ngOnInit(): void {
     this.subscription = this.routingService.onChange.subscribe(paths => {
       this.breadcrumbs = paths;
     });
   }
 
-  /**
-   * @method ngOnDestroy
-   */
   ngOnDestroy(): void {
-    this.subscription.unsubscribe();
+    if (this.subscription) {
+      this.subscription.unsubscribe();
+    }
   }
 }
